6ac0f952af1c8795cad68e14f8c5f4f5234dc8ba
Kathy Ellison <kaellison@corelogic.com>
2018-10-25 08:28:52 -0500

    Demo Maven Project - initial version

---
+++ mavendemo/src/test/java/com/learnmaven/mavendemo/AppTest.java
@@ -0,0 +1,53 @@
+package com.learnmaven.mavendemo;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public AppTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( AppTest.class );
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testAdd()
+    {
+        assertEquals(10, new Calculator().add(5,5));
+    }
+    
+    public void testSubtract()
+    {
+        assertEquals(10, new Calculator().subtract(15,5));
+    }
+    
+    public void testMultiply()
+    {
+        assertEquals(10, new Calculator().multiply(5, 20));
+    }
+    
+    public void testDivide()
+    {
+        assertEquals(10.0, new Calculator().divide(100, 10));
+    }
+}
